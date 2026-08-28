01f42119280a7275fc83aca429c9b301fd20fffb
abdullah14120 <abdullah.tamemii@gmail.com>
2026-08-28 15:34:53 +0300

    Update default port and socket connection timeout
    Changed default port from 9988 to 8888 and adjusted socket connection timeout from 2500ms to 2000ms.

--- app/src/main/kotlin/com/file/whatsapp/core/ConnectionManager.kt
+++ app/src/main/kotlin/com/file/whatsapp/core/ConnectionManager.kt
@@ -10,7 +10,7 @@ import java.net.Socket
 
 object ConnectionManager {
 
-    const val DEFAULT_PORT = 9988
+    const val DEFAULT_PORT = 8888
 
     enum class Role { SENDER, RECEIVER }
     enum class Mode { WIFI, USB }
@@ -23,7 +23,7 @@ object ConnectionManager {
     suspend fun testSocketConnection(ip: String, port: Int = DEFAULT_PORT): Boolean = withContext(Dispatchers.IO) {
         return@withContext try {
             Socket().use { socket ->
-                socket.connect(InetSocketAddress(ip, port), 2500)
+                socket.connect(InetSocketAddress(ip, port), 2000)
                 socket.isConnected
             }
         } catch (e: Exception) {
@@ -33,6 +33,7 @@ object ConnectionManager {
 
     suspend fun startReceiverServer(port: Int = DEFAULT_PORT, onClientConnected: (Socket) -> Unit) = withContext(Dispatchers.IO) {
         ServerSocket(port).use { serverSocket ->
+            serverSocket.soTimeout = 0
             val clientSocket = serverSocket.accept()
             onClientConnected(clientSocket)
         }
